c23caf25b41db034dfd63e48d975a552a711ead6
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-06 22:42:56 +0300

    Wrong preparing of

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,23 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import initialState from './initialState';
 import tablesReducer from './tablesRedux';
+import { compose } from 'redux';
 
 const subreducers = {
   tables: tablesReducer,
 };
 
 const reducer = combineReducers(subreducers);
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(),
-  compose(applyMiddleware(thunk))
-);
+
+const middlewares = [thunk];
+const middlewareEnhancer = applyMiddleware(...middlewares);
+const composeDevTool = composeWithDevTools();
+
+const enhancers = [middlewareEnhancer, composeDevTool];
+const composedEnhancers = compose(...enhancers);
+
+const store = createStore(reducer, initialState, composedEnhancers);
 
 export default store;
