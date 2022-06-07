edf133a5d3908593de57c743d2b7596f5c858583
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-07 14:36:57 +0300

    Trying fix store.js v2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import initialState from './initialState';
@@ -9,14 +9,16 @@ const subreducers = {
 };
 
 const reducer = combineReducers(subreducers);
+const store = createStore(
+  reducer,
+  initialState,
 
-const middleware = { thunk };
-const middlewareEnhancer = applyMiddleware(...middleware);
-const composeDevTool = composeWithDevTools();
-
-const enhancers = [middlewareEnhancer, composeDevTool];
-const composedEnhancers = compose(...enhancers);
-
-const store = createStore(reducer, initialState, composedEnhancers);
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+);
 
 export default store;
