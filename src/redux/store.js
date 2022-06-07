1214a17d9d3765d21312ce162823a4024c1edf9c
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-07 10:15:34 +0300

    Found non-callable @@iterator

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import initialState from './initialState';
 import tablesReducer from './tablesRedux';
@@ -9,11 +9,14 @@ const subreducers = {
 };
 
 const reducer = combineReducers(subreducers);
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(),
-  compose(applyMiddleware(thunk))
-);
+
+const middleware = { thunk };
+const middlewareEnhancer = applyMiddleware(...middleware);
+const composeDevTool = composeWithDevTools();
+
+const enhancers = [middlewareEnhancer, composeDevTool];
+const composedEnhancers = compose(...enhancers);
+
+const store = createStore(reducer, initialState, composedEnhancers);
 
 export default store;
