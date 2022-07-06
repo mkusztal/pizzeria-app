72b3e00231da971fc7bec701c6195bbffa3c96bf
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-06 22:31:58 +0300

    Correct prepare server and links

--- src/dist/config.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.API_URL = void 0;
-var API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3131/api';
-exports.API_URL = API_URL;
